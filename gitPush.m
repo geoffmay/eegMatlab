edda28a84e8f6ab98ce0375636bdec4f966357de
Geoff May <geomay@gmail.com>
2018-03-27 10:05:24 -0500

    gitPush.m-desktop-u9jmn35

--- gitPush.m
+++ gitPush.m
@@ -5,9 +5,13 @@ oldfolder = pwd;
 cd(fileparts(which('gitPush')));
 [error1, message1] = system('git add --all');
 if(error1)
-    error(sprintf('error using gitAdd: %s', message1));
+    error(sprintf('error using "git add": %s', message1));
 end
 a = sprintf('git commit -am %s', message);
-[out3, out4] = system(a);
+[error2, message2] = system(a);
+if(error2)
+    error(sprintf('error using "git commit": %s', message2));
+end
+
 [out5, out6] = system('git push');
 cd(oldfolder);
